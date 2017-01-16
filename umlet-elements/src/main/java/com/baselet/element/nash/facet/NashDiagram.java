e1f1bbcef9716e0447ebdfedad957f48bebd90f2
Matthias Lupberger <matthiaslupberger@hotmail.de>
2017-01-16 14:53:45 +0100

    Added possible names for structogramms

--- umlet-elements/src/main/java/com/baselet/element/nash/facet/NashDiagram.java
+++ umlet-elements/src/main/java/com/baselet/element/nash/facet/NashDiagram.java
@@ -3,6 +3,7 @@ package com.baselet.element.nash.facet;
 import java.util.List;
 
 import com.baselet.control.basics.geom.DimensionDouble;
+import com.baselet.control.enums.AlignHorizontal;
 import com.baselet.diagram.draw.DrawHandler;
 import com.baselet.element.facet.PropertiesParserState;
 
@@ -11,16 +12,36 @@ public class NashDiagram {
 	NashContainer algorithm;
 	DimensionDouble dimension;
 
+	String name;
+	boolean hasName;
+
 	public NashDiagram(List<String> code) {
-		algorithm = new NashContainer(code);
+		if (code.size() >= 1 && code.get(0).startsWith("name=")) {
+			name = code.get(0).substring(5).trim();
+			hasName = true;
+			algorithm = new NashContainer(code.subList(1, code.size()));
+		}
+		else {
+			algorithm = new NashContainer(code);
+			hasName = false;
+		}
 	}
 
 	public DimensionDouble getDimension(DrawHandler drawer) {
 		dimension = algorithm.calculateDimension(drawer);
+		if (hasName) {
+			dimension = new DimensionDouble(dimension.getWidth(), dimension.getHeight() + 45.0);
+		}
 		return dimension;
 	}
 
 	public void draw(DrawHandler drawer, PropertiesParserState state) {
-		algorithm.draw(drawer, state, 0, 0, dimension.getWidth());
+		double yOffset = 0;
+
+		if (hasName) {
+			drawer.print(name, 0, 45.0 / 2.0 + drawer.getDistanceBorderToText() + drawer.textHeight(name), AlignHorizontal.LEFT);
+			yOffset += 45.0;
+		}
+		algorithm.draw(drawer, state, 0, yOffset, dimension.getWidth());
 	}
 }
