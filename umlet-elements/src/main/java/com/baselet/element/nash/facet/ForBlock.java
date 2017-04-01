abf3e2eac7795511c091af9840f625e91ce2b122
Matthias Lupberger <matthiaslupberger@hotmail.de>
2017-04-01 11:01:11 +0200

    Added For-Loop to Structogramm

---
+++ umlet-elements/src/main/java/com/baselet/element/nash/facet/ForBlock.java
@@ -0,0 +1,54 @@
+package com.baselet.element.nash.facet;
+
+import java.util.List;
+
+import com.baselet.control.basics.geom.DimensionDouble;
+import com.baselet.control.enums.AlignHorizontal;
+import com.baselet.diagram.draw.DrawHandler;
+import com.baselet.element.facet.PropertiesParserState;
+
+public class ForBlock implements Containable {
+	private final String condition;
+	private final NashContainer commands;
+
+	private final double drawHeight = 45.0;
+	private final double drawWidth = 45.0;
+
+	private double height = 0;
+
+	public ForBlock(String condition, List<String> code) {
+		this.condition = condition;
+		commands = new NashContainer(code);
+	}
+
+	@Override
+	public DimensionDouble calculateDimension(DrawHandler drawer) {
+		DimensionDouble dimension = commands.calculateDimension(drawer);
+
+		double width = dimension.getWidth() + drawWidth;
+		height = dimension.getHeight() + drawHeight;
+
+		return new DimensionDouble(width, height);
+	}
+
+	@Override
+	public void draw(DrawHandler drawer, PropertiesParserState state, double xOffset, double yOffset, double width) {
+		drawer.drawLine(xOffset, yOffset, xOffset, yOffset + height);
+		drawer.drawLine(xOffset, yOffset, width, yOffset);
+		drawer.drawLine(width, yOffset, width, yOffset + drawHeight);
+		drawer.drawLine(width, yOffset + drawHeight, xOffset + drawWidth, yOffset + drawHeight);
+		drawer.drawLine(xOffset + drawWidth, yOffset + drawHeight, xOffset + drawWidth, yOffset + height);
+		drawer.drawLine(xOffset + drawWidth, yOffset + height, xOffset, yOffset + height);
+
+		drawer.print(condition, xOffset + drawWidth / 2.0, yOffset + drawer.getDistanceBorderToText() * 2 + drawer.textHeight(condition) + drawer.getDistanceBetweenTextLines(), AlignHorizontal.LEFT);
+
+		commands.draw(drawer, state, xOffset + drawWidth, yOffset + drawHeight, width);
+
+	}
+
+	@Override
+	public double getHeight() {
+		return height;
+	}
+
+}
